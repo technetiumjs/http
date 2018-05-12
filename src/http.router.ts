4c7f9d6b7b6d2432837536309af84c28cf72edbe
John A. Fedoruk <johnny@johnfedoruk.ca>
2018-05-12 00:00:38 -0500

    Added HttpRouter

---
+++ src/http.router.ts
@@ -0,0 +1,20 @@
+import { Route, Router } from "@yellow-snow/core";
+import * as express from "express";
+import { NextFunction } from "express-serve-static-core";
+import { HttpRoute } from "./http.route";
+
+export class HttpRouter extends Router {
+    public init(router: express.Router) {
+        this.routes.forEach((route: Route<any>) => {
+            if (route instanceof HttpRoute) {
+                router[route.verb](
+                    route.path,
+                    (req: express.Request, res: express.Response, next: NextFunction) => {
+                        const ctrl = new (route.controller)(req, res, next);
+                        ctrl[route.method]();
+                    },
+                );
+            }
+        });
+    }
+}
